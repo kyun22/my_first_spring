b403f9c6ae20268d9db345a105c82dbe3fad3962
kyun22 <seokhyun90@gmail.com>
2020-09-28 23:26:28 +0900

    4 dat of EOC2

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
-@Controller 
+@Controller
 public class MemberController {
     //    private final MemberService memberService = new MemberService(); // 이렇게 쓰면 하나의 객체를 공용으로 쓸 수 없다.
     private final MemberService memberService;
