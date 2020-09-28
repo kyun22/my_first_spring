5715ad1c4e2f19edfee416ae68cf825d051f6ebd
kyun22 <seokhyun90@gmail.com>
2020-09-28 23:03:16 +0900

    4 dat of EOC

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller 
+public class MemberController {
+    //    private final MemberService memberService = new MemberService(); // 이렇게 쓰면 하나의 객체를 공용으로 쓸 수 없다.
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();  // 동시성 문제 고려할 경우 : 컨커런트 해쉬
